c56c6996a6cdf0cf6df2bab63efde14ed7203ba2
peterige2004 <51337786+peterige2004@users.noreply.github.com>
2023-09-09 19:59:31 +0100

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayofTheWeek"]');
 const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
 
-const now = new Date();
+function updateCurrentTime() {
+    const now = new Date();
 
-// Display the current day of the week
-currentDayOfTheWeek.textContent = `Current Day of the Week: ${now.toLocaleDateString('en-US', { weekday: 'long' })}`;
+    // Display the current day of the week
+    currentDayOfTheWeek.textContent = `Current Day of the Week: ${now.toLocaleDateString('en-US', { weekday: 'long' })}`;
 
-// Display the current time in milliseconds
-const currentTimeInMilliseconds = now.getTime();
-currentUTCTime.textContent = `Current Time in Milliseconds: ${currentTimeInMilliseconds}`;
+    // Display the current time in milliseconds
+    const currentTimeInMilliseconds = now.getTime();
+    currentUTCTime.textContent = `Current Time in Milliseconds: ${currentTimeInMilliseconds}`;
+}
+
+updateCurrentTime(); // Update initially
+setInterval(updateCurrentTime, 1000); // Update every 1000 milliseconds (1 second)

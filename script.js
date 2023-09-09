985453a109445176c0a3fe01ea57063fbe1c39d3
peterige2004 <51337786+peterige2004@users.noreply.github.com>
2023-09-09 20:14:14 +0100

    script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function updateCurrentTime() {
     const now = new Date();
 
     // Display the current day of the week
-    currentDayOfTheWeek.textContent = `Current Day of the Week: ${now.toLocaleDateString('en-US', { weekday: 'long' })}`;
+    currentDayOfTheWeek.textContent = now.toLocaleDateString('en-US', { weekday: 'long' })
 
     // Display the current time in milliseconds
     const currentTimeInMilliseconds = now.getTime();
-    currentUTCTime.textContent = `Current Time in Milliseconds: ${currentTimeInMilliseconds}`;
+    currentUTCTime.textContent = currentTimeInMilliseconds
 }
 
 updateCurrentTime(); // Update initially

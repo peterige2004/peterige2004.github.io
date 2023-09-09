8e1f1869ca6d634b59640ffd792e4aed4305efce
peterige2004 <51337786+peterige2004@users.noreply.github.com>
2023-09-09 19:27:21 +0100

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const currentDayOfTheWeek = document.getElementById('currentDayofTheWeek');
-const currentUTCTime = document.getElementById('currentUTCTime');
+document.addEventListener("DOMContentLoaded", function() {
+    const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
 
-const now = new Date();
+    const now = new Date();
 
-// Display the current day of the week
-currentDayOfTheWeek.innerHTML = now.toLocaleDateString('en-US', { weekday: 'long' });
+    // Display the current day of the week
+    currentDayOfTheWeek.textContent = now.toLocaleDateString('en-US', { weekday: 'long' });
 
-// Display the current time in milliseconds
-const currentTimeInMilliseconds = now.getTime();
-currentUTCTime.innerHTML = `Current Time in Milliseconds: ${currentTimeInMilliseconds}`;
+    // Display the current time in milliseconds
+    const currentTimeInMilliseconds = now.getTime();
+    currentUTCTime.textContent = `Current Time in Milliseconds: ${currentTimeInMilliseconds}`;
+});
